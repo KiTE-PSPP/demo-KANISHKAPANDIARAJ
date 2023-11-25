b377a974c4bc26114d047eb2712bf33da7d6b888
KANISHKAPANDIARAJ <147989567+KANISHKAPANDIARAJ@users.noreply.github.com>
2023-11-25 21:38:42 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
-    a=6*9
+    a=6 * 9
 def global_answer():
-    a=6*7
+    a=6 * 7
     print("the value of a inside the fuction is",a)
-    global_answer():
+    global_answer()
     print("the value of a outside the function is",a)
